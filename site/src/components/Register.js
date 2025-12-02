c227684c513e6eb5b3692ac9701e973cdb1202c5
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-12-02 21:31:50 +0200

    Add register component

--- site/src/components/Register.js
+++ site/src/components/Register.js
@@ -1,11 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+import { register } from '../services/register';
+
 export default function Register() {
+    const navigate = useNavigate();
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const {email, password, rePass} = Object.fromEntries(formData);
+
+        if(password != rePass){
+            alert("Паролите не съвпадат!");
+            return;
+        };
+
+        let result = await register(email, password);
+
+        if(result.status == 200){
+            alert("Успешно влизане!");
+            navigate('/');
+        }else{
+            alert("Възникна грешка при влизане!");
+            navigate('/login');
+        }
+    }
     return (
         <div class="container">
-            <h1 style={{marginBottom: "1rem"}}>Регистрация</h1>
-            <form>
-                <input type="text" placeholder="Имейл" required />
-                <input type="password" placeholder="Парола" required />
-                <input type="password" placeholder="Повторете паролата" required />
+            <h1 style={{ marginBottom: "1rem" }}>Регистрация</h1>
+            <form onSubmit={loginHandler}>
+                <input type="text" name="email" placeholder="Имейл" required />
+                <input type="password" name="password" placeholder="Парола" required />
+                <input type="password" name="rePass" placeholder="Повторете паролата" required />
 
                 <button>Качи проекта</button>
             </form>
